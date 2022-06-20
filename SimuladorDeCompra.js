9aafe56cd2e249bf82183f612e805ed59a236488
bautistasaggese <bautistasaggese2002@gmail.com>
2022-06-20 04:35:44 -0300

    version 2.2 animacion proyecto bug solucionado

--- SimuladorDeCompra.js
+++ SimuladorDeCompra.js
@@ -36,16 +36,39 @@
 // }
 
 
-
-//animaciones
+//
+//animaciones PROCESO DE DESARROLLO
+//
 window.addEventListener('scroll', function(){
     let animacion = document.getElementById('Proceso1');
     let posicionObj1 = animacion.getBoundingClientRect().top;
     console.log(posicionObj1);
-    let tamañoDePantalla = window.innerHeight/3;
+    let tamañoDePantalla = window.innerHeight;
 
     if(posicionObj1 < tamañoDePantalla){
-        animacion.style.animation = 'mover 1s ease-out'
+        animacion.style.animation = 'mover 1.2s ease-in-out'
     }
 })
 
+window.addEventListener('scroll', function(){
+    let animacion = document.getElementById('Proceso2');
+    let posicionObj1 = animacion.getBoundingClientRect().top;
+    console.log(posicionObj1);
+    let tamañoDePantalla = window.innerHeight;
+
+    if(posicionObj1 < tamañoDePantalla){
+        animacion.style.animation = 'mover 1.2s ease-in-out'
+    }
+})
+
+window.addEventListener('scroll', function(){
+    let animacion = document.getElementById('Proceso3');
+    let posicionObj1 = animacion.getBoundingClientRect().top;
+    console.log(posicionObj1);
+    let tamañoDePantalla = window.innerHeight;
+
+    if(posicionObj1 < tamañoDePantalla){
+        animacion.style.animation = 'mover 1.2s ease-in-out'
+    }
+})
+//FIN DE ANIMACIONES PROYECTOS DE DESARROLLO
