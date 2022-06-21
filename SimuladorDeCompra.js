75c93f8dfcf82e23c028d8f5d35f38f6148a726d
bautistasaggese <bautistasaggese2002@gmail.com>
2022-06-21 19:03:35 -0300

    version 2.5 con proceso de compra para coder

--- SimuladorDeCompra.js
+++ SimuladorDeCompra.js
@@ -47,7 +47,7 @@ window.addEventListener('scroll', function(){
     let animacion = document.getElementById('Proceso1');
     let posicionObj1 = animacion.getBoundingClientRect().top;
     console.log(posicionObj1);
-    let tamañoDePantalla = window.innerHeight*2;
+    let tamañoDePantalla = window.innerHeight/1.2;
 
     if(posicionObj1 < tamañoDePantalla){
         animacion.style.animation = 'mover 1.2s ease-in-out'
@@ -58,7 +58,7 @@ window.addEventListener('scroll', function(){
     let animacion = document.getElementById('Proceso2');
     let posicionObj1 = animacion.getBoundingClientRect().top;
     console.log(posicionObj1);
-    let tamañoDePantalla = window.innerHeight*2;
+    let tamañoDePantalla = window.innerHeight/1.2;
 
     if(posicionObj1 < tamañoDePantalla){
         animacion.style.animation = 'mover 1.2s ease-in-out'
@@ -69,7 +69,7 @@ window.addEventListener('scroll', function(){
     let animacion = document.getElementById('Proceso3');
     let posicionObj1 = animacion.getBoundingClientRect().top;
     console.log(posicionObj1);
-    let tamañoDePantalla = window.innerHeight*2;
+    let tamañoDePantalla = window.innerHeight/1.2;
 
     if(posicionObj1 < tamañoDePantalla){
         animacion.style.animation = 'mover 1.2s ease-in-out'
