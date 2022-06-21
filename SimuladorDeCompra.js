a8b8d2f575e543b55a9795e89097a8930b61483c
bautistasaggese <bautistasaggese2002@gmail.com>
2022-06-21 16:42:21 -0300

    version 2.5 corrigiendo error de nav id

--- SimuladorDeCompra.js
+++ SimuladorDeCompra.js
@@ -80,7 +80,7 @@ window.addEventListener('scroll', function(){
     let animacion = document.getElementById('mantenimiento1');
     let posicionObj1 = animacion.getBoundingClientRect().top;
     console.log(posicionObj1);
-    let tamañoDePantalla = window.innerHeight*2;
+    let tamañoDePantalla = window.innerHeight/1.2;
 
     if(posicionObj1 < tamañoDePantalla){
         animacion.style.animation = 'mover 1.2s ease-in-out'
