24b315439ab183bfd67ebe22c5125d22f26e6e54
bautistasaggese <bautistasaggese2002@gmail.com>
2022-06-22 03:35:49 -0300

    version 2.6 se arregla bug de id que redirecciona para arriba

--- SimuladorDeCompra.js
+++ SimuladorDeCompra.js
@@ -1,42 +1,42 @@
 //Proceso de compra
 
-let quisocomprar = "si";
-let producto = "pagina";
-let precio = 0;
-let cantidad = 0;
-let precioTotal = 0;
-let seguirComprando = false;
+// let quisocomprar = "si";
+// let producto = "pagina";
+// let precio = 0;
+// let cantidad = 0;
+// let precioTotal = 0;
+// let seguirComprando = false;
 
-let deseacomprar = prompt("¿Desea comprar algun producto? Conteste con si o no.");
+// let deseacomprar = prompt("¿Desea comprar algun producto? Conteste con si o no.");
 
-if (deseacomprar == quisocomprar){
-    do{
-        producto = prompt ("Queres comprar una pagina, mantenimiento de pagina o ambos? Conteste con minusculas!!!, pagina, mantenimiento o ambos.");
-        cantidad = Number(prompt("Cuantos queres comprar?"));
+// if (deseacomprar == quisocomprar){
+//     do{
+//         producto = prompt ("Queres comprar una pagina, mantenimiento de pagina o ambos? Conteste con minusculas!!!, pagina, mantenimiento o ambos.");
+//         cantidad = Number(prompt("Cuantos queres comprar?"));
     
-        switch (producto) {
-            case "pagina":
-                precio = 90;
-                break;
-            case "mantenimiento":
-            precio = 25;
-            case "ambos":
-                precio = 115;
-            default:
-                alert("Algunos de los datos ingresados no son correctos")
-                precio = 0;
-                cantidad = 0;
-                break;
-        }
-        precioTotal += precio * cantidad;
+//         switch (producto) {
+//             case "pagina":
+//                 precio = 90;
+//                 break;
+//             case "mantenimiento":
+//             precio = 25;
+//             case "ambos":
+//                 precio = 115;
+//             default:
+//                 alert("Algunos de los datos ingresados no son correctos")
+//                 precio = 0;
+//                 cantidad = 0;
+//                 break;
+//         }
+//         precioTotal += precio * cantidad;
     
-        seguirComprando = confirm("Queres seguir comprando?")
-    }while (seguirComprando);
+//         seguirComprando = confirm("Queres seguir comprando?")
+//     }while (seguirComprando);
     
-    alert ("El valor de la compra es: $"+precioTotal+". Para terminar la compra aprete el boton CONTACTANOS en el menu de navegacion." );
-}else{
-    alert("Disfrute nuestro sitio web")
-}
+//     alert ("El valor de la compra es: $"+precioTotal+". Para terminar la compra aprete el boton CONTACTANOS en el menu de navegacion." );
+// }else{
+//     alert("Disfrute nuestro sitio web")
+// }
 
 //Fin de proceso de compra
 
@@ -47,7 +47,7 @@ window.addEventListener('scroll', function(){
     let animacion = document.getElementById('Proceso1');
     let posicionObj1 = animacion.getBoundingClientRect().top;
     console.log(posicionObj1);
-    let tamañoDePantalla = window.innerHeight/1.2;
+    let tamañoDePantalla = window.innerHeight;
 
     if(posicionObj1 < tamañoDePantalla){
         animacion.style.animation = 'mover 1.2s ease-in-out'
@@ -58,7 +58,7 @@ window.addEventListener('scroll', function(){
     let animacion = document.getElementById('Proceso2');
     let posicionObj1 = animacion.getBoundingClientRect().top;
     console.log(posicionObj1);
-    let tamañoDePantalla = window.innerHeight/1.2;
+    let tamañoDePantalla = window.innerHeight;
 
     if(posicionObj1 < tamañoDePantalla){
         animacion.style.animation = 'mover 1.2s ease-in-out'
@@ -69,7 +69,7 @@ window.addEventListener('scroll', function(){
     let animacion = document.getElementById('Proceso3');
     let posicionObj1 = animacion.getBoundingClientRect().top;
     console.log(posicionObj1);
-    let tamañoDePantalla = window.innerHeight/1.2;
+    let tamañoDePantalla = window.innerHeight;
 
     if(posicionObj1 < tamañoDePantalla){
         animacion.style.animation = 'mover 1.2s ease-in-out'
@@ -85,7 +85,7 @@ window.addEventListener('scroll', function(){
     let animacion = document.getElementById('mantenimiento1');
     let posicionObj1 = animacion.getBoundingClientRect().top;
     console.log(posicionObj1);
-    let tamañoDePantalla = window.innerHeight/1.2;
+    let tamañoDePantalla = window.innerHeight;
 
     if(posicionObj1 < tamañoDePantalla){
         animacion.style.animation = 'mover 1.2s ease-in-out'
